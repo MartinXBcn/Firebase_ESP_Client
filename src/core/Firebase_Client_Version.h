0084f064d2600c9b70802406e14901607253f0be
Martin <martinxbcn@gmail.com>
2024-04-20 13:14:45 +0200

    New adjustment to IDF 5.1.3.

--- src/core/Firebase_Client_Version.h
+++ src/core/Firebase_Client_Version.h
@@ -5,6 +5,6 @@
 
 // <MS>
 // To check in using program if correct version of forked library is used.
-#define MS_FIREBASE_ESP_CLIENT_FORK_VERSION_INT 240041600
+#define MS_FIREBASE_ESP_CLIENT_FORK_VERSION_INT 240042000
 
 #endif
